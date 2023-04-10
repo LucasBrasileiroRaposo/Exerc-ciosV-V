02c93a5c60c173456ed3fcc16a7f24a70f2f3468
LucasBrasileiroRaposo <lucas.raposo@ccc.ufcg.edu.br>
2023-04-10 15:36:50 -0300

    criar funcionário genéricos

---
+++ CalculadoraDeSalario/test/FuncionarioTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FuncionarioTest {
+
+    private final double  SALARIO_MEDIO_DEV = 3000;
+    private final double  SALARIO_MEDIO_DBA = 2000;
+    private final double  SALARIO_MEDIO_TESTADOR = 2000;
+    private final double  SALARIO_MEDIO_GERENTE = 5000;
+    @BeforeEach
+    public void setUp() {
+        Funcionario funcionarioDev = new Funcionario("Lucas brasileiro", "lucas@gmail.com", SALARIO_MEDIO_DEV, DESENVOLVEDOR);
+        Funcionario funcionarioDBA = new Funcionario("Holliver costa", "holliver@gmail.com", SALARIO_MEDIO_DBA, DBA);
+        Funcionario funcionarioTestador = new Funcionario("Italo silva", "italo@gmail.com",SALARIO_MEDIO_TESTADOR , TESTADOR);
+        Funcionario funcionarioGerente = new Funcionario("Felipe santos", "felipe@gmail.com",SALARIO_MEDIO_GERENTE , GERENTE);
+    }
+
+
+}
