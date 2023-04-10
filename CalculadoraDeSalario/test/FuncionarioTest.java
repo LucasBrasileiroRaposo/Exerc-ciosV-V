fa4acadc7b06cf0404831fc690b56322fa079307
LucasBrasileiroRaposo <lucas.raposo@ccc.ufcg.edu.br>
2023-04-10 17:14:03 -0300

    ajustes finais classe de teste, apenas detalhes de nomenclatura e lógica de implementação

--- CalculadoraDeSalario/test/FuncionarioTest.java
+++ CalculadoraDeSalario/test/FuncionarioTest.java
@@ -15,11 +15,44 @@ public class FuncionarioTest {
     private CalculadoraSalarioImpl calculadoraSalario = new CalculadoraSalarioImpl();
     @BeforeEach
     public void setUp() {
-        Funcionario funcionarioDev = new Funcionario("Lucas brasileiro", "lucas@gmail.com", SALARIO_MEDIO_DEV, DESENVOLVEDOR);
-        Funcionario funcionarioDBA = new Funcionario("Holliver costa", "holliver@gmail.com", SALARIO_MEDIO_DBA, DBA);
-        Funcionario funcionarioTestador = new Funcionario("Italo silva", "italo@gmail.com",SALARIO_MEDIO_TESTADOR , TESTADOR);
-        Funcionario funcionarioGerente = new Funcionario("Felipe santos", "felipe@gmail.com",SALARIO_MEDIO_GERENTE , GERENTE);
+        funcionario = new Funcionario("Lucas brasileiro", "lucas@gmail.com", SALARIO_MEDIO_DEV, Cargo.DESENVOLVEDOR);
     }
-
+    @Test
+    public void calculaSalarioDev(){
+        assertEquals(2400,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals(SALARIO_MEDIO_DEV*0.8,calculadoraSalario.calculaSalario(funcionario));
+        funcionario.setSalarioBase(SALARIO_MEDIO_DEV-1);
+        assertEquals(2699.1,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals((SALARIO_MEDIO_DEV-1)*0.9,calculadoraSalario.calculaSalario(funcionario));
+    }
+    @Test
+    public void calculaSalarioDBA(){
+        funcionario.setCargo(Cargo.DBA);
+        funcionario.setSalarioBase(SALARIO_MEDIO_DBA);
+        assertEquals(1500,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals(SALARIO_MEDIO_DBA*0.75,calculadoraSalario.calculaSalario(funcionario));
+        funcionario.setSalarioBase(SALARIO_MEDIO_DBA/2);
+        assertEquals(850,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals((SALARIO_MEDIO_DBA/2)*0.85,calculadoraSalario.calculaSalario(funcionario));
+    }
+    @Test
+    public void calculaSalarioTestador(){
+        funcionario.setCargo(Cargo.TESTADOR);
+        funcionario.setSalarioBase(SALARIO_MEDIO_TESTADOR);
+        assertEquals(1500,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals(SALARIO_MEDIO_TESTADOR*0.75,calculadoraSalario.calculaSalario(funcionario));
+        funcionario.setSalarioBase(SALARIO_MEDIO_TESTADOR/2);
+        assertEquals(850,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals((SALARIO_MEDIO_TESTADOR/2)*0.85,calculadoraSalario.calculaSalario(funcionario));
+    }
+    @Test
+    public void calculaSalarioGerente(){
+        funcionario.setCargo(Cargo.GERENTE);
+        funcionario.setSalarioBase(SALARIO_MEDIO_GERENTE);
+        assertEquals(3500,calculadoraSalario.calculaSalario(funcionario));
+        assertEquals(SALARIO_MEDIO_GERENTE*0.70,calculadoraSalario.calculaSalario(funcionario));
+        funcionario.setSalarioBase(SALARIO_MEDIO_GERENTE-1);
+        assertEquals((SALARIO_MEDIO_GERENTE-1)*0.80,calculadoraSalario.calculaSalario(funcionario));
+    }
 
 }
